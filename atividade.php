63ced07f83c98bbc8e355fdc6015dd0303663fd2
Lara-Senac <laraestudante187@gmail.com>
2024-08-26 21:02:56 -0300

    Add files via upload

---
+++ atividade.php
@@ -0,0 +1,14 @@
+
+<?php
+$idade = 70;
+if($idade >= 65){
+    echo "O uso tem que ser com restrições";
+}
+
+elseif($idade >= 16){
+echo "Consegue indigerir o médicamento";
+}
+else{
+    echo "É proibido o uso de médicamentos";
+}
+?>
